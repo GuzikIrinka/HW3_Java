aaed00462426275548b33542a2895ed6a4bce89f
Irina <andrey_k.90@mail.ru>
2022-11-28 00:27:07 +0300

    Задача сортировка слиянием

---
+++ sort.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class sort {
+    public static int[] mergeSort(int[] sortArr) {
+        int[] buf1 = Arrays.copyOf(sortArr, sortArr.length);
+        int[] buf2 = new int[sortArr.length];
+        int[] result = mergeSortInner(buf1, buf2, 0, sortArr.length);
+        return result;
+    }
+
+    public static int[] mergeSortInner(int[] buf1, int[] buf2, int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buf1;
+        }
+
+        // уже отсортирован
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergeSortInner(buf1, buf2, startIndex, middle);
+        int[] sorted2 = mergeSortInner(buf1, buf2, middle, endIndex);
+
+        // слияние
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buf1 ? buf2 : buf1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++]
+                    : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
+
+    public static void main(String args[]) {
+        int[] sortArr = { 2, 16, 24, 8, 5, 11, 3, 9 };
+        int[] result = mergeSort(sortArr);
+        System.out.println(Arrays.toString(result));
+    }
+}
